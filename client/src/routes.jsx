db77948b405e2f33b1ac069c2c34804a83f940af
James Grieve <pocojames@hotmail.com>
2022-01-07 14:01:00 -0800

    create homepage folder

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./components/Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Homepage/Home';
 
 const AppRoutes = () => {
   return (
