a7baa8b261b9b47d967411ebae2b1ad803241175
James Grieve <pocojames@hotmail.com>
2022-01-08 00:13:31 -0800

    add boxes to card for title, body and user values

--- client/src/components/Homepage/PostCard.jsx
+++ client/src/components/Homepage/PostCard.jsx
@@ -5,12 +5,44 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+const data = {
+  id: 1,
+  name: 'User',
+  time: '1:35 PM PST',
+  topic: 'funny',
+  body: 'this is an example message for this app, this is where the user can post his brilliant ideas/thoughts/jokes/other',
+};
+
+const bull = (
+  <Box
+    component="span"
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      mx: '3px',
+      transform: 'scale(1.5)',
+    }}
+  >
+    •
+  </Box>
+);
 
 const PostCard = () => {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography>Card Example</Typography>
+        <Box>
+          <Typography variant="h5">> {data.topic}</Typography>
+        </Box>
+        <Box display="flex" paddingX={1}>
+          <Box>
+            <Typography component="p">{data.name}</Typography>
+          </Box>
+          {bull}
+          <Box>
+            <Typography component="p">{data.time}</Typography>
+          </Box>
+        </Box>
       </CardContent>
     </Card>
   );
