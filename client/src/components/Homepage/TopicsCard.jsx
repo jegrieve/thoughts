20d8d2f815d84fb7a28f5bc02453a611b678aa40
James Grieve <pocojames@hotmail.com>
2022-01-08 13:35:31 -0800

    add basic layout structuring of TopicsCard content

--- client/src/components/Homepage/TopicsCard.jsx
+++ client/src/components/Homepage/TopicsCard.jsx
@@ -5,12 +5,38 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 const TopicsCard = () => {
+  const data = [
+    { name: 'funny' },
+    { name: 'news' },
+    { name: 'sports' },
+    { name: 'random' },
+    { name: 'dev' },
+    { name: 'funny' },
+    { name: 'news' },
+    { name: 'sports' },
+    { name: 'random' },
+    { name: 'dev' },
+  ];
   return (
-    <Card variant="outlined" sx={{ minWidth: 275 }}>
+    <Card
+      variant="outlined"
+      sx={{ minWidth: 275, marginTop: 2, maxWidth: 450 }}
+    >
       <CardContent>
-        <Typography>Topics</Typography>
+        <Grid container spacing={1}>
+          {data.map((data) => {
+            return (
+              <Grid item xs={6}>
+                <Box>
+                  <Typography>{data.name}</Typography>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
       </CardContent>
     </Card>
   );
