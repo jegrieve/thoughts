9704c376045a0cc180768c63f0acd0ca5279133c
James Grieve <pocojames@hotmail.com>
2022-01-08 14:12:19 -0800

    add textfield to homepage

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -4,15 +4,25 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import PostFeed from './PostFeed';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 const Home = () => {
   return (
     <div className="homepage-container">
       <Container>
         <Grid container spacing={5}>
           <Grid item xs={12} md={6}>
-            <Typography variant="h3" component="div">
-              Topics
-            </Typography>
+            <Box>
+              <Typography variant="h3" component="span">
+                Topics
+              </Typography>
+              <TextField
+                id="standard-basic"
+                label="Standard"
+                variant="standard"
+              />
+            </Box>
+
             <TopicsCard />
           </Grid>
           <Grid item xs={12} md={6}>
