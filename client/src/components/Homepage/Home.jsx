4e9f309af984188e8b8ba7c3e65ce8086493474d
James Grieve <pocojames@hotmail.com>
2022-01-07 20:32:10 -0800

    make homepage layout responsive

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import TopicsCard from './TopicsCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import PostFeed from './PostFeed';
 const Home = () => {
   return (
-    <div>
-      <TopicsCard />
+    <div className="homepage-container">
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={6}>
+            <TopicsCard />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <PostFeed />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
