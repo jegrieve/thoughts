491eafeed2a36f6a539666f010fe2bad723b2e7e
James Grieve <pocojames@hotmail.com>
2022-01-08 00:23:23 -0800

    add load-more btn to postfeed

--- client/src/components/Homepage/PostFeed.jsx
+++ client/src/components/Homepage/PostFeed.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PostCard from './PostCard';
+import Button from '@mui/material/Button';
+
 const PostFeed = () => {
   return (
     <div>
       <PostCard />
+      <Button>Load More</Button>
     </div>
   );
 };
