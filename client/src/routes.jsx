12100a874a34a915c42938eff4a478418bf630db
James Grieve <pocojames@hotmail.com>
2022-01-08 20:46:14 -0800

    add signin and signup pages to routes

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Homepage/Home';
 import Navbar from './components/Navbar/Navbar';
+import SignIn from './components/Session/SignIn';
+import SignUp from './components/Session/SignUp';
+
 const AppRoutes = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
   );
