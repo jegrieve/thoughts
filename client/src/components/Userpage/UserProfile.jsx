20ee4f063ecd8d0050456227f805b04b782a3890
James Grieve <pocojames@hotmail.com>
2022-01-09 02:03:50 -0800

    create user profile component

---
+++ client/src/components/Userpage/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostCard from './PostCard';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Select from '@mui/material/Select';
+
+const UserProfile = () => {
+  return <div>UserProfile</div>;
+};
+
+export default UserProfile;
