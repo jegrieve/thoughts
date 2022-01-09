b3235c7c1b6b7297abdfbe726d76198d2f37954e
James Grieve <pocojames@hotmail.com>
2022-01-09 02:23:05 -0800

    add basic layout to userpage

--- client/src/components/Userpage/UserProfile.jsx
+++ client/src/components/Userpage/UserProfile.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  return (
+    <Container>
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={6}>
+          <Box>User stuff</Box>
+          <Box>User created time</Box>
+          <Box>User bio stuff</Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box>User's PostFeed</Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default UserProfile;
