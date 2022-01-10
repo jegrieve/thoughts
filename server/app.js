6318f3ecf4fa4522b088b4ccd468961d688a2160
James Grieve <pocojames@hotmail.com>
2022-01-09 23:04:07 -0800

    add GET route for users and specific user

--- server/app.js
+++ server/app.js
@@ -18,6 +18,30 @@ app.post('/users', async (req, res) => {
   }
 });
 
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.get('/users/:uuid', async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    const user = await User.findOne({
+      where: { uuid },
+    });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
 app.listen({ port: 5000 }, async () => {
   console.log('Server running on localhost:5000');
   await sequelize.authenticate();
