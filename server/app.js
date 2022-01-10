6565a3042564c6441226dd1387437254e21ffaa8
James Grieve <pocojames@hotmail.com>
2022-01-09 20:42:05 -0800

    install express

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
+const express = require('express');
+
 const { sequelize } = require('./models');
+
+const app = express();
+
+async function main() {
+  await sequelize.sync({ force: true });
+}
+
+main();
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -12,15 +10,30 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    bio: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      bio: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'users',
+      modelName: 'User',
+    }
+  );
   return User;
 };
