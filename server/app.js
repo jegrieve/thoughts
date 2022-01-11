4e21bf4811f1f2a7949324ffdde5d0b92d7ee107
James Grieve <pocojames@hotmail.com>
2022-01-11 15:37:21 -0800

    add api router

--- server/app.js
+++ server/app.js
@@ -1,46 +1,22 @@
 const express = require('express');
-
-const { sequelize, User } = require('./models');
+const api = require('./api');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const { sequelize } = require('./models');
+const bodyParser = require('body-parser');
+require('./auth/passport');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
-  const { name, email, password, bio } = req.body;
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
-  try {
-    const user = await User.create({ name, email, password, bio });
-
-    return res.json(user);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.findAll();
-
-    return res.json(users);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-app.get('/users/:uuid', async (req, res) => {
-  const uuid = req.params.uuid;
-  try {
-    const user = await User.findOne({
-      where: { uuid },
-    });
-    return res.json(user);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
-  }
-});
+app.use('/api/v1', api);
 
 app.listen({ port: 5000 }, async () => {
   console.log('Server running on localhost:5000');
