53fa6660436188027210419ce552a2d1209fb303
James Grieve <pocojames@hotmail.com>
2022-01-11 20:25:27 -0800

    fix bcrypt bug with login

--- server/api/login.js
+++ server/api/login.js
@@ -17,20 +17,21 @@ router.post('/login', async (req, res) => {
       .status(400)
       .json({ message: 'Email or password does not match!' });
 
-  bcrypt.compare(password, user.password, (err, res) => {
-    if (!res) {
+  bcrypt.compare(password, user.password, (err, match) => {
+    if (!match) {
       res.json({ message: 'Incorrect password' });
+    } else {
+      const jwtToken = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET
+      );
+
+      // const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET); //to decode
+
+      res.json({ message: 'Welcome Back!', token: jwtToken, user });
     }
   });
-
-  const jwtToken = jwt.sign(
-    { id: user.id, username: user.username },
-    process.env.JWT_SECRET
-  );
-
-  // const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET); //to decode
-
-  res.json({ message: 'Welcome Back!', token: jwtToken, user });
+  res.json({ message: 'Something went wrong.' });
 });
 
 module.exports = router;
