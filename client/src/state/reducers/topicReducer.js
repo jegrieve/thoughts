15ca2c1140b7462245819f621fa0180b29776f62
James Grieve <pocojames@hotmail.com>
2022-01-13 00:51:33 -0800

    remove uncessary case

--- client/src/state/reducers/topicReducer.js
+++ client/src/state/reducers/topicReducer.js
@@ -2,8 +2,6 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case 'setTopics':
       return action.payload;
-    case 'setMainTopic':
-      return action.payload;
     case 'appendTopic':
       return [...action, action.payload];
     default:
