d1019b226a67746aff07505ac5975ba9c257b7ef
James Grieve <pocojames@hotmail.com>
2022-01-12 19:47:14 -0800

    add create topic post request

--- client/src/components/Homepage/CreateTopic.jsx
+++ client/src/components/Homepage/CreateTopic.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,8 +6,53 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import { FormControl } from '@mui/material';
+import axios from 'axios';
 
 const CreateTopic = () => {
+  const [createTopicInput, setCreateTopicInput] = useState({ name: '' });
+  const [formError, setFormError] = useState(false);
+
+  const enterCreateTopicInput = (e) => {
+    setCreateTopicInput((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitCreateTopicInputs = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('SavedToken');
+    axios
+      .post(
+        'http://localhost:5000/api/v1/create-topic',
+        {
+          name: createTopicInput['name'],
+        },
+        {
+          headers: { Authorization: token },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        if (response.data.uuid) {
+          //if user not signed in, we should disable btns,
+          handleFormSubmitSucess();
+        } else {
+          if (response.data.errors) {
+            setFormError(true);
+          }
+        }
+      })
+      .catch(function (error) {
+        setFormError(true);
+      });
+  };
+
+  const handleFormSubmitSucess = () => {
+    setFormError(false);
+    //set states of topic and post to the submitted topic.
+  };
+
   return (
     <Card
       variant="outlined"
@@ -20,9 +65,13 @@ const CreateTopic = () => {
           </Typography>
         </Box>
         <Box>
-          <form>
+          <form onSubmit={submitCreateTopicInputs}>
             <FormControl>
               <TextField
+                name="name"
+                error={formError}
+                onChange={enterCreateTopicInput}
+                value={createTopicInput['name']}
                 required
                 InputLabelProps={{ required: false }}
                 id="create-topic-home"
