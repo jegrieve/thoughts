b801800af2b33f15a3ccade43149755f639fb096
James Grieve <pocojames@hotmail.com>
2022-01-12 16:17:36 -0800

    add get and post routes for topic

--- server/api/topic.js
+++ server/api/topic.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const passport = require('passport');
+const { Topic } = require('../models');
 const router = express.Router();
 
 router.get(
-  '/topic',
+  '/topics',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.send('authenticated');
+  async (req, res) => {
+    try {
+      const topics = await Topic.findAll();
+      return res.json(topics);
+    } catch (err) {
+      return res.status(500).json({ error: 'Something went wrong' });
+    }
+  }
+);
+
+router.post(
+  '/create-topic',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const { name } = req.body;
+
+    try {
+      const topic = await Topic.create({ name });
+
+      return res.json(topic);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   }
 );
 
