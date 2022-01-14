9aff103bc2889ad066ae807a0fc9220582da09b6
James Grieve <pocojames@hotmail.com>
2022-01-14 15:28:37 -0800

    create post model

---
+++ server/models/post.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Topic }) {
+      // define association here
+      thisBelongsTo(User, { foreignKey: 'userId' });
+      thisBelongsTo(Topic, { foreignKey: 'topicId' });
+    }
+  }
+  Post.init(
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      topicId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'posts',
+      modelName: 'Post',
+    }
+  );
+  return Post;
+};
