5c9afc5921aa6616d227a6aeb723f95bf5e79f94
James Grieve <pocojames@hotmail.com>
2022-01-17 03:44:45 -0800

    style userpage

--- client/src/components/Userpage/UserProfile.jsx
+++ client/src/components/Userpage/UserProfile.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import PostCard from '../Homepage/PostCard';
+import Typography from '@mui/material/Typography';
 
 const UserProfile = (props) => {
   const [userPosts, setUserPosts] = useState([]);
@@ -40,11 +38,10 @@ const UserProfile = (props) => {
   return (
     <Container>
       <Grid container spacing={5}>
-        <Grid item xs={12} md={12}>
-          <Box>User name</Box>
-        </Grid>
-        <Grid item xs={12} md={12}>
-          <Box>Recent Posts:</Box>
+        <Grid item xs={12} md={12} marginTop={3}>
+          <Box>
+            <Typography variant="h5">Recent Thoughts:</Typography>
+          </Box>
         </Grid>
         <Grid item xs={12} md={6}>
           {userPosts.map((data) => {
