63b5941c5471991899d19924d135fb1a2fd40f87
James Grieve <pocojames@hotmail.com>
2022-01-17 20:16:33 -0800

    remove payment route

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const registerApi = require('./register');
 const loginApi = require('./login');
-const paymentApi = require('./payment');
 const postApi = require('./post');
 const topicApi = require('./topic');
 
@@ -9,7 +8,6 @@ const router = express.Router();
 
 router.use(registerApi);
 router.use(loginApi);
-router.use(paymentApi);
 router.use(postApi);
 router.use(topicApi);
 
